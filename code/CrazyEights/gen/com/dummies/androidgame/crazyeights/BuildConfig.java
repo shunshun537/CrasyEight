1522488fe380c6ec8765ee10e71497c2349ee09d
Jason Sun <sjingshun@yahoo.cn>
2013-03-26 22:25:06 +0800

    Add crazy 8s code to git

---
+++ code/CrazyEights/gen/com/dummies/androidgame/crazyeights/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dummies.androidgame.crazyeights;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
